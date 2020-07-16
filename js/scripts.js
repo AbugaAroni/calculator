37b489e3ef8709870b438a21eaf72edecfca26f2
AbugaAroni <abugaaroni@gmail.com>
2020-07-16 15:54:29 +0300

    will add sub, div, mult functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
+
+//add fuction
 var add = function(number1, number2) {
   return number1 + number2;
 };
+
+//subtract function
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+//multiply function
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+//divide function
+var divid = function(number1, number2) {
+  return number1 / number2;
+};
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-alert( add (number1, number2));
+alert("The addition " + add (number1, number2)+  " " + "The subtraction " + sub(number1, number2) + " " + "The multiplication " + mult(number1, number2) + " " + "The divison " + divid(number1, number2)  );
